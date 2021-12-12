c7f30d62e0facfaf490d89ab3702dc928f1d0700
mmfei <wlfkongl@163.com>
2021-12-13 01:24:12 +0800

    修正post格式

--- src/Handler/FeiShu/PostFormatter.php
+++ src/Handler/FeiShu/PostFormatter.php
@@ -9,7 +9,7 @@ use Alarm\Handler\AbstractTextFormatter;
 
 /**
  * 富文本
- * @see https://open.feishu.cn/document/uAjLw4CM/ukTMukTMukTM/reference/im-v1/message/create
+ * @see https://open.feishu.cn/document/uAjLw4CM/ukTMukTMukTM/im-v1/message/create_json
  * @package Alarm\Handler\FeiShu
  */
 class PostFormatter extends AbstractTextFormatter
@@ -20,21 +20,57 @@ class PostFormatter extends AbstractTextFormatter
      */
     public function format(Record $record): array
     {
-        $data = parent::format($record);
+        $result = [
+            'msg_type' => 'post',
+            'content'  => [],
+        ];
+        $content = sprintf('[%s] %s: %s', $record->datetime->format('Y-m-d H:i:s'), $record->level, $record->message);
+
+        $message_arr = $record->message ? json_decode($record->message, true) : [];
+        $is_return = false;
+        if (isset($message_arr['zh_cn']['title']) && ($message_arr['zh_cn']['content'] ?? [])) {
+            $result['content']['post']['zh_cn']['title']   = $message_arr['zh_cn']['title'];
+            $result['content']['post']['zh_cn']['content'] = $message_arr['zh_cn']['content'];
+            $is_return                             = true;
+        }
+        if (isset($message_arr['en_us']['title']) && ($message_arr['en_us']['content'] ?? [])) {
+            $result['content']['post']['en_us']['title']   = $message_arr['en_us']['title'];
+            $result['content']['post']['en_us']['content'] = $message_arr['en_us']['content'];
+            $is_return                             = true;
+        }
+        return $is_return ? $result : [];
+
+    }
+    /**
+     * 举个例子.
+     * @return array
+     */
+    private function getDemo()
+    {
         return [
             'msg_type' => 'post',
-            'content' => [
-                'text' => $data['content'],
+            'content'  => [
+                'post' => [
+                    'zh_cn' => [
+                        'title'   => '',
+                        'content' => [
+                            [
+                                ['tag' => 'text', 'text' => '第一行'],
+                                ['tag' => 'a', 'text' => '第一个链接', 'href' => 'https://open.feishu.cn/'],
+                            ],
+                        ],
+                    ],
+                    'en_us' => [
+                        'title'   => '',
+                        'content' => [
+                            [
+                                ['tag' => 'text', 'text' => '第一行'],
+                                ['tag' => 'a', 'text' => '第一个链接', 'href' => 'https://open.feishu.cn/'],
+                            ],
+                        ],
+                    ],
+                ],
             ],
         ];
-//        https://open.feishu.cn/document/uAjLw4CM/ukTMukTMukTM/reference/im-v1/message/create
-//       curl --location --request POST 'https://open.feishu.cn/open-apis/im/v1/messages?receive_id_type=chat_id' \
-//--header 'Authorization: Bearer t-XXX' \
-//--header 'Content-Type: application/json; charset=utf-8' \
-//--data-raw '{
-//    "receive_id": "oc_84983ff6516d731e5b5f68d4ea2e1da5",
-//    "content": "{\"text\":\"<at user_id=\\\"ou_155184d1e73cbfb8973e5a9e698e74f2\\\">Tom</at>  d \"}",
-//    "msg_type": "text"
-//}'
     }
 }
